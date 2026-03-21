5af9e0487ad2eca4ed9854f52d0e06d21df361fb
Sameer <sameersk2080@gmail.com>
2026-03-21 17:42:32 +0530

    added VRAM logs to gitignore

--- custom-pipeline/main.py
+++ custom-pipeline/main.py
@@ -1,8 +1,10 @@
 from AutomatedPipeline import menus
 from AutomatedPipeline import file_handler as fh
+
 SIBR_APP__PATH_VARIABLE="sibr_viewer"
 DATASET_PATH_VARIABLE="dataset_path"
 OUTPUT_PATH_VARIABLE="output_path"
+
 def main():
     data=fh.get_paths()
     fh.check_paths(data)
